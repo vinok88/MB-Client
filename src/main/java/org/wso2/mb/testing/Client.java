db5006bb78380f51e910f7ed126d2173f92e36b8
Asanka Abeyweera <asankaab@wso2.com>
2014-11-11 11:09:57 +0530

    Add Simple message Queue sender and receiver

---
+++ src/main/java/org/wso2/mb/testing/Client.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2005-2014, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.wso2.mb.testing;
+
+import org.apache.log4j.Logger;
+import org.wso2.mb.testing.jms.AndesQueuePublisher;
+import org.wso2.mb.testing.jms.AndesQueueSubscriber;
+
+import javax.jms.JMSException;
+import javax.naming.NamingException;
+
+/**
+ * Main class
+ */
+public class Client {
+    private static Logger log = Logger.getLogger(Client.class);
+
+    public static void main(String[] args) {
+        String action = args[0];
+
+        try {
+            if ("receive".equals(action)) {
+                AndesQueueSubscriber queueClient = new AndesQueueSubscriber.Builder("admin", "admin", "TestQueue")
+                        .build();
+                queueClient.connect();
+                log.info(queueClient.receive());
+            } else if ("send".equals(action)) {
+                AndesQueuePublisher queueClient = new AndesQueuePublisher.Builder("admin", "admin", "TestQueue")
+                        .build();
+                queueClient.connect();
+                queueClient.send("Test Message");
+            }
+        } catch (NamingException e) {
+            log.error("Error while creating receiving client. Please check given arguments.", e);
+        } catch (JMSException e) {
+            log.error("Error occured while receiving message", e);
+        }
+    }
+}
